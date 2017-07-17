0d85d44cb34057c43dcc88b8a948f9d73f2b306d
Sylvestre <sylvestre.antoine@vodeclic.com>
2017-07-17 10:42:46 +0200

    Http module

--- README.md
+++ README.md
@@ -8,4 +8,6 @@ RUN : node src/server.js
 
 VISIT :
 
-*  curl http://localhost:8080/
+* lsof -i :8080 | grep node | awk '{print $2}' | xargs kill -9; node src/server.js &; sleep 1; curl http://localhost:8080
+
+* lsof -i :8080 | grep node | awk '{print $2}' | xargs kill -9; node src/server.js &; sleep 1; curl http://localhost:8080\?year=2017\&month=July
--- src/server.js
+++ src/server.js
@@ -1,6 +1,13 @@
 var http = require('http')
+var url = require('url');
 
-http.createServer(function(req, res) {
+//create a server object:
+http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+  res.write('Hello World!');
+  res.write('Running the query : ', req.url);
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+}).listen(8080); //the server object listens on port 8080
+
