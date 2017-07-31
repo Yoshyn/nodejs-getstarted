5005420e9693d870afdddd32eb0afa9ef36316d1
Sylvestre <sylvestre.antoine@vodeclic.com>
2017-07-31 14:49:21 +0200

    filesystem

---
+++ src/node_file_system.js
@@ -0,0 +1,36 @@
+// Create empty file
+var fs = require('fs');
+fs.open('mynewfile2.txt', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+//Create file
+var fs = require('fs');
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+// Append file
+var fs = require('fs');
+fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
+  if (err) throw err;
+  console.log('Updated!');
+});
+// Replace file and content
+var fs = require('fs');
+fs.writeFile('mynewfile3.txt', 'This is my text', function (err) {
+  if (err) throw err;
+  console.log('Replaced!');
+});
+// Delete file
+var fs = require('fs');
+fs.unlink('mynewfile2.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
+});
+// Rename file
+var fs = require('fs');
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+  if (err) throw err;
+  console.log('File Renamed!');
+});
--- src/server.js
+++ src/server.js
@@ -1,13 +1,17 @@
 var http = require('http')
 var url = require('url');
+var fs = require('fs');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('Hello World!');
-  res.write('Running the query : ', req.url);
+  console.log('Running the query : ', req.url);
   var q = url.parse(req.url, true).query;
-  var txt = q.year + " " + q.month;
-  res.end(txt);
+  // console.log(q.year);
+  fs.readFile('./src/hello.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 }).listen(8080); //the server object listens on port 8080
+
 
